bb8568717624854caacf1a2315454bed85823c70
Carina43 <cag003@edu.zealand.dk>
2025-05-15 10:18:16 +0200

    Animalrepo's

--- Dyreinternatet/Repository/AnimalRepo.cs
+++ Dyreinternatet/Repository/AnimalRepo.cs
@@ -1,7 +1,22 @@
-﻿namespace Dyreinternatet.Repository
+﻿using Dyreinternatet.Model;
+namespace Dyreinternatet.Repository
 {
     public class AnimalRepo:IAnimalRepo
     {
+
+        List<Animal> _animals;
+
+        protected List<Animal> Animals
+        {
+            get { return _animals; }
+            set { _animals = value; }
+        }
+
+        public List<Animal> GetAll()
+        {
+            return Animals;
+        }
+
         public void Add()
         {
 
--- Dyreinternatet/Repository/IAnimalRepo.cs
+++ Dyreinternatet/Repository/IAnimalRepo.cs
@@ -1,6 +1,11 @@
-﻿namespace Dyreinternatet.Repository
+﻿using Dyreinternatet.Model;
+namespace Dyreinternatet.Repository
 {
     public interface IAnimalRepo
     {
+
+        List<Animal> GetAll();
+        void Add();
+        void Remove();
     }
 }
