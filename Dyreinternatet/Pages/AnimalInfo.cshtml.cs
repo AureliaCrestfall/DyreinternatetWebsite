f22c27ae5fdada1e142d500b0766bafb22bc1876
thecorf <mim010@edu.zealand.dk>
2025-05-15 12:14:50 +0200

    Added blank pages

---
+++ Dyreinternatet/Pages/AnimalInfo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dyreinternatet.Pages
+{
+    public class AnimalInfoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
