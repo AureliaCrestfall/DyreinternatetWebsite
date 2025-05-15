4693448b8dc5abde919d1bac5f4a0c28cb2ced5a
crestfall <mas025@edu.zealand.dk>
2025-05-15 10:08:49 +0200

    added using model to activtyRepo

--- Dyreinternatet/Repository/ActivityRepo.cs
+++ Dyreinternatet/Repository/ActivityRepo.cs
@@ -1,13 +1,14 @@
-﻿namespace Dyreinternatet.Repository
+﻿using Dyreinternatet.Model;
+namespace Dyreinternatet.Repository
 {
     public class ActivityRepo:IActivityRepo
     {
-
+        
         List<Activity> _activities;
 
         protected List<Activity> Activities
         {
-            get { return _activities}
+            get { return _activities; }
             set { _activities = value; }
         }
 
