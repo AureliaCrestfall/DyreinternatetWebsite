ad7e44ca90452c8e4c116ffb7d24da29ba9d6e01
crestfall <mas025@edu.zealand.dk>
2025-05-15 10:44:46 +0200

    added meoder to activity service

--- Dyreinternatet/Service/ActivityService.cs
+++ Dyreinternatet/Service/ActivityService.cs
@@ -1,9 +1,25 @@
-﻿namespace Dyreinternatet.Service
+﻿using Dyreinternatet.Model;
+using Dyreinternatet.Repository;
+
+namespace Dyreinternatet.Service
 {
     public class ActivityService
     {
 
+        IActivityRepo _activityRepo;
 
+        public void Add(Activity activity)
+        {
+            _activityRepo.Add(activity);
+        }
+        public void Remove(int title)
+        {
+            _activityRepo.Remove(title);
+        }
+        public List<Activity> GetAll()
+        {
+            return _activityRepo.GetAll();
+        }
 
     }
 }
