287e6cda5b43412b7c8c4461f07bc60fcee5ef70
thecorf <mim010@edu.zealand.dk>
2025-05-19 12:04:44 +0200

    Made workerpage functional again

--- Dyreinternatet/Pages/WorkerPage.cshtml.cs
+++ Dyreinternatet/Pages/WorkerPage.cshtml.cs
@@ -1,3 +1,5 @@
+using Dyreinternatet.Model;
+using Dyreinternatet.Service;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +7,19 @@ namespace Dyreinternatet.Pages
 {
     public class WorkerModel : PageModel
     {
+
+        private readonly WorkerService _workers;
+
+        [BindProperty]
+        public List<Worker> Workers { set; get; }
+        public WorkerModel(WorkerService workerS)
+        {
+            Workers = workerS.GetAll();
+
+            _workers = workerS;
+        }
+
+
         public void OnGet()
         {
         }
